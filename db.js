217e727cfe7a39be406af354cfc3c88b409ce30d
rithi <ritimahesh29@gmail.com>
2022-01-24 11:15:48 +0530

    remove try catch

--- db.js
+++ db.js
@@ -5,11 +5,8 @@ module.exports = () => {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   };
-  try {
-    mongoose.connect(process.env.DB, connectionParams);
+
+  mongoose.connect(process.env.DB, connectionParams, () => {
     console.log("connected to database succesfully");
-  } catch (error) {
-    console.log(error);
-    console.log("could not connect to database!");
-  }
+  });
 };
