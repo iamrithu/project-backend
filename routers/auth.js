e344d59d11ccab1942f5ff2e7fe99bb33e2f60fb
rithi <ritimahesh29@gmail.com>
2022-01-24 12:39:30 +0530

    error

--- routers/auth.js
+++ routers/auth.js
@@ -4,26 +4,29 @@ const { User } = require("../models/user");
 const bcrypt = require("bcrypt");
 
 router.post("/auth", async (req, res) => {
-  // try {
-  const { error } = validate(req.body);
-  if (error) {
-    return res.status(400).send({ message: error.details[0].message });
-  }
-  const user = await User.findOne({ email: req.body.email });
+  try {
+    const { error } = validate(req.body);
+    if (error) {
+      return res.status(400).send({ message: error.details[0].message });
+    }
+    const user = await User.findOne({ email: req.body.email });
 
-  if (!user) {
-    return res.status(401).send({ message: "Invalid Email or Password" });
-  }
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) {
-    return res.status(401).send({ message: "Invalid Email or Password" });
-  }
-  const token = user.generateToken();
+    if (!user) {
+      return res.status(401).send({ message: "Invalid Email or Password" });
+    }
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.status(401).send({ message: "Invalid Email or Password" });
+    }
+    const token = user.generateToken();
 
-  res.status(200).send({ data: token, message: "Logged in successfully" });
-  // } catch (error) {
-  //   res.status(500).send({ message: "Internal Server Error" });
-  // }
+    res.status(200).send({ data: token, message: "Logged in successfully" });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
 const validate = (data) => {
