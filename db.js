3e6d4fc05d31583fb603b492af54818da73954ff
rithi <ritimahesh29@gmail.com>
2022-01-24 11:03:15 +0530

    heroku

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  // const connectionParams = {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true,
-  // };
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
   try {
-    mongoose.connect(process.env.DB);
+    mongoose.connect(process.env.DB, connectionParams);
     console.log("connected to database succesfully");
   } catch (error) {
     console.log(error);
